452dbccd52a59fc3ff2200c485d0ad333bd61d6f
azuminka <peckovastanka@gmail.com>
2023-06-02 23:10:12 +0200

    feat: proxy server configuration

--- pripojenie_sietoveho_zariadenia/move_device.c
+++ pripojenie_sietoveho_zariadenia/move_device.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE     //Povolenie GNU rozsirenie
 #include <stdlib.h>     //EXIT_FAILURE
 #include <stdio.h>      //sprintf(), perror(), printf()
+#include <unistd.h>     // access()
 
 int main(int argc, char *argv[]) {
     char cmd[256];
@@ -8,6 +9,13 @@ int main(int argc, char *argv[]) {
     //Program ocakava dva argumenty: nazov menneho priestoru a nazov sietoveho zariadenia
     if (argc != 3) {
         printf("Použitie: %s [sieťový_menný_priestor] [sieťové_zariadenie]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    // Definujeme cesty k suborom, ktore reprezentuju sietove menne priestory
+    char path[256];
+    sprintf(path, "/var/run/netns/%s", argv[1]);
+    if (access(path, F_OK) == -1) {
+        perror("Sieťový menný priestor neexistuje");
         return EXIT_FAILURE;
     }
     //Priprava prikazu na presun sietoveho zariadenia do menneho priestoru
