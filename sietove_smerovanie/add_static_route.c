8df3a5bb4e285feee81953316eb51599f71ac0fd
azuminka <peckovastanka@gmail.com>
2023-06-02 19:27:57 +0200

    fixed: add and delete

--- sietove_smerovanie/add_static_route.c
+++ sietove_smerovanie/add_static_route.c
@@ -2,6 +2,16 @@
 #include <unistd.h>     //access()
 #include <stdlib.h>     //EXIT_FAILURE
 #include <stdio.h>      //sprintf(), perror(), printf()
+#include <arpa/inet.h>  //inet_aton()
+#include <string.h>     // funkciu strtok()
+
+// Kontrola, ci je hodnota masky v platnom rozsahu
+int valid_mask(int mask) {
+    if (mask >= 0 && mask <= 32) {
+        return 1;
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -16,6 +26,21 @@ int main(int argc, char *argv[]) {
     sprintf(path, "/var/run/netns/%s", argv[1]);
     if(access(path, F_OK) != 0) {
         printf("Sieťový menný priestor %s neexistuje\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+    // Oddelenie IP adresy a masky
+    char *ip_address = strtok(argv[2], "/");
+    char *mask_str = strtok(NULL, "/");
+    
+    // Kontrola, či sú IP adresa a maska v platných formátoch
+    struct sockaddr_in sa;
+    if (inet_pton(AF_INET, ip_address, &(sa.sin_addr)) == 0) {
+        printf("Neplatná IP adresa\n");
+        return EXIT_FAILURE;
+    }
+    int mask = atoi(mask_str);
+    if (!valid_mask(mask)) {
+        printf("Neplatná maska podsiete\n");
         return EXIT_FAILURE;
     }
 
@@ -33,5 +58,9 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
+
+
+
 
 
--- sietove_smerovanie/delete_static_route.c
+++ sietove_smerovanie/delete_static_route.c
@@ -2,6 +2,16 @@
 #include <unistd.h> // access()
 #include <stdlib.h> //printf(), perror(), sprintf()
 #include <stdio.h>  //printf(), perror(), sprintf()
+#include <arpa/inet.h>  //inet_aton()
+#include <string.h>     // funkciu strtok()
+
+// Kontrola, ci je hodnota masky v platnom rozsahu
+int valid_mask(int mask) {
+    if (mask >= 0 && mask <= 32) {
+        return 1;
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -11,6 +21,22 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
     
+    // Oddelenie IP adresy a masky
+    char *ip_address = strtok(argv[2], "/");
+    char *mask_str = strtok(NULL, "/");
+    
+    // Kontrola, či sú IP adresa a maska v platných formátoch
+    struct sockaddr_in sa;
+    if (inet_pton(AF_INET, ip_address, &(sa.sin_addr)) == 0) {
+        printf("Neplatná IP adresa\n");
+        return EXIT_FAILURE;
+    }
+    int mask = atoi(mask_str);
+    if (!valid_mask(mask)) {
+        printf("Neplatná maska podsiete\n");
+        return EXIT_FAILURE;
+    }
+
     char cmd[256];
     int ret;
 
